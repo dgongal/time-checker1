94cfb2169b879ef6ad89ac019426cc18e9a53743
dgongal <dipika@fusemachines.com>
2017-07-06 23:57:15 -0400

    create one api that allows users to view time from different cities

---
+++ src/main/java/timechecker/TimeCheckerApplication.java
@@ -0,0 +1,12 @@
+package timechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeCheckerApplication.class, args);
+	}
+}
